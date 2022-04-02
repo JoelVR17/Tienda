2845eba95dcdbe75a24ca5ffb90c022df59a580b
JoelVR17 <vermudo.com@gmail.com>
2022-04-02 16:24:20 -0600

    Semana 10

--- src/main/java/com/tienda/SecurityConfig.java
+++ src/main/java/com/tienda/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.tienda;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -25,6 +26,31 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .withUser("pedro")
                 .password("{noop}123")
                 .roles("USER");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/articulo/nuevo", "/articulo/guardar",
+                        "/articulo/modificar/**", "/articulo/eliminar/**",
+                        "/categoria/nuevo", "/categoria/guardar",
+                        "/categoria/modificar/**", "/categoria/eliminar/**",
+                        "/cliente/nuevo", "/cliente/guardar",
+                        "/cliente/modificar/**", "/cliente/eliminar/**",
+                        "/usuario/listado",
+                        "/usuario/nuevo", "/usuario/guardar",
+                        "/usuario/modificar/**", "/usuario/eliminar/**")
+                .hasRole("ADMIN")
+                .antMatchers("/articulo/listado", "/categoria/listado",
+                        "/cliente/listado")
+                .hasAnyRole("ADMIN", "VENDEDOR")
+                .antMatchers("/")
+                .hasAnyRole("USER", "VENDEDOR", "ADMIN")
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .and()
+                .exceptionHandling().accessDeniedPage("/errores/403");
     }
 
 }
--- src/main/java/com/tienda/WebConfig.java
+++ src/main/java/com/tienda/WebConfig.java
@@ -1,4 +1,3 @@
-
 package com.tienda;
 
 import java.util.Locale;
@@ -6,36 +5,44 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer{
+public class WebConfig implements WebMvcConfigurer {
 
     @Bean
     public LocaleResolver localeResolver() {
-        
+
         var slr = new SessionLocaleResolver();
         slr.setDefaultLocale(new Locale("es"));
         return slr;
-        
+
     }
-    
+
     @Bean
     public LocaleChangeInterceptor localeChageInterceptor() {
-        
+
         var lci = new LocaleChangeInterceptor();
         lci.setParamName("lang");
         return lci;
-        
+
     }
-    
+
     @Override
     public void addInterceptors(InterceptorRegistry registro) {
-        
+
         registro.addInterceptor(localeChageInterceptor());
-        
+
     }
-    
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registro) {
+        registro.addViewController("/").setViewName("index");
+        registro.addViewController("/login");
+        registro.addViewController("/errores/403").setViewName("/errores/403");
+    }
+
 }
